f097fbdd1bbbdb753aba54009563a60aa9744abe
André Fragoso <andrefsantosprog@gmail.com>
2024-07-13 12:01:32 -0300

    Criação das classes Usuarios e Tarefas

---
+++ src/main/java/com/tarefas/api/entities/Tarefa.java
@@ -0,0 +1,33 @@
+package com.tarefas.api.entities;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_tarefas")
+public class Tarefa {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+    
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @Column(nullable = false)
+    private LocalDate dataEntrega;
+    
+}
